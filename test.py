6e6f8d5a7ec0c74958e964aee8cb7da7b8284084
ry4b76c7mb-create <ry4b76c7mb@privaterelay.appleid.com>
2026-09-17 22:12:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import requests
-from html.parser import HTMLParser
 from urllib.parse import quote
 
-
 print("沈药校园信息雷达")
-print("开始侦察：沈药学工……")
+print("开始检查百度返回内容……")
 print()
 
 query = 'site:mp.weixin.qq.com/s/ "沈药学工"'
@@ -13,8 +11,8 @@ url = "https://www.baidu.com/s?wd=" + quote(query)
 headers = {
     "User-Agent": (
         "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) "
-        "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 "
-        "Mobile/15E148 Safari/604.1"
+        "AppleWebKit/605.1.15 (KHTML, like Gecko) "
+        "Version/17.0 Mobile/15E148 Safari/604.1"
     )
 }
 
@@ -25,57 +23,34 @@ try:
         timeout=15
     )
 
-    print("百度访问状态码：", response.status_code)
-    print("网页长度：", len(response.text), "字符")
-    print()
-
     html = response.text
 
-    class LinkParser(HTMLParser):
-        def __init__(self):
-            super().__init__()
-            self.links = []
-            self.current_href = None
-            self.current_text = []
-
-        def handle_starttag(self, tag, attrs):
-            if tag == "a":
-                attrs = dict(attrs)
-                self.current_href = attrs.get("href")
-                self.current_text = []
-
-        def handle_data(self, data):
-            if self.current_href is not None:
-                self.current_text.append(data)
-
-        def handle_endtag(self, tag):
-            if tag == "a" and self.current_href:
-                text = "".join(self.current_text).strip()
-                self.links.append((text, self.current_href))
-                self.current_href = None
-                self.current_text = []
-
-    parser = LinkParser()
-    parser.feed(html)
-
-    print("搜索结果中的链接数量：", len(parser.links))
+    print("百度状态码：", response.status_code)
+    print("网页长度：", len(html), "字符")
     print()
-    print("其中包含 mp.weixin.qq.com 的结果：")
 
-    count = 0
+    checks = [
+        "沈药学工",
+        "mp.weixin.qq.com",
+        "百度安全验证",
+        "验证码",
+        "安全验证",
+        "访问异常",
+        "请完成验证",
+    ]
 
-    for text, href in parser.links:
-        if "mp.weixin.qq.com" in href or "mp.weixin.qq.com" in text:
-            count += 1
-            print()
-            print("【结果", count, "】")
-            print("标题：", text[:200])
-            print("链接：", href[:500])
+    print("关键词检查：")
+
+    for word in checks:
+        print(word, "→", "找到" if word in html else "没有")
 
     print()
-    print("最终找到微信相关链接：", count, "个")
+    print("百度返回内容前 1000 个字符：")
+    print("--------------------------------")
+    print(html[:1000])
+    print("--------------------------------")
     print()
-    print("侦察结束。")
+    print("检查结束。")
 
 except Exception as e:
     print("发生错误：", type(e).__name__)
